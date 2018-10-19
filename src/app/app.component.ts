471d7ddfe1121ce455daf7166a70842c927f6bf7
KevReece <kevinjamesreece@gmail.com>
2018-10-19 12:51:43 +0200

    get console lines from app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Dungeon';
+  consoleLines = ['AppComponent started']
+
+  constructor(){
+    this.consoleLines.push('AppComponent constructor started')
+  }
 }
--- src/app/user-console/user-console.component.ts
+++ src/app/user-console/user-console.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-console',
@@ -7,13 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserConsoleComponent implements OnInit {
 
-  lines: string[] = []
+  @Input() lines: string[];
 
   constructor() { 
-    this.lines.push("constructor called")
   }
 
   ngOnInit() {
-    this.lines.push("ngOnInit called")
   }
 }
