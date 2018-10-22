8a9ca20a02277ea300f5bb6bc0d39cd12eb8d558
KevReece <kevinjamesreece@gmail.com>
2018-10-22 10:35:38 +0200

    e2e loading tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -5,10 +5,22 @@ describe('workspace-project App', () => {
 
   beforeEach(() => {
     page = new AppPage();
+    page.navigateTo();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
+  it('should load app', () => {
     expect(page.getParagraphText()).toEqual('Welcome to Dungeon!');
+  });
+
+  it('should load charactor component', () => {
+    expect(page.getCharactorComponent().isPresent()).toBeTruthy();
+  });
+
+  it('should load map component', () => {
+    expect(page.getMapComponent().isPresent()).toBeTruthy();
+  });
+
+  it('should load user console component', () => {
+    expect(page.getUserConsoleComponent().isPresent()).toBeTruthy();
   });
 });
