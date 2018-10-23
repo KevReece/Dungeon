ce1c91d8aab86e0cdcfac945f3d71545fc00f816
KevReece <kevinjamesreece@gmail.com>
2018-10-23 15:53:57 +0200

    random gold quantity

---
+++ src/app/model/cellitems/gold.model.spec.ts
@@ -0,0 +1,14 @@
+import { Gold } from "./gold.model";
+
+describe('Gold', () => {
+    describe('constructor', () => {
+        it('should have random quantity from 10 to 100 and a multiple of 10', () => {
+            const gold = new Gold();
+            console.log(gold.quantity);
+
+            expect(gold.quantity).toBeGreaterThanOrEqual(10);
+            expect(gold.quantity).toBeLessThanOrEqual(100);
+            expect(gold.quantity % 10).toEqual(0);
+        });
+    });
+});
--- src/app/model/cellitems/gold.model.ts
+++ src/app/model/cellitems/gold.model.ts
@@ -1,5 +1,12 @@
 import { ICellItem } from './i-cell-item.model';
 
 export class Gold implements ICellItem {
-    quantity: Number;
+    quantity: number;
+
+    constructor(){
+        const quantityMultipleOf = 10;
+        const minimumQuantity = 10;
+        const maximumQuantity = 100;
+        this.quantity = Math.floor(((Math.random() * maximumQuantity) + minimumQuantity) / quantityMultipleOf) * quantityMultipleOf;
+    }
 }
